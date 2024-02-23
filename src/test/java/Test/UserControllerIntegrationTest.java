1321cd9a30d21aded394590a7e9570e1a36a7c8d
Reece Nunez <reecenunez@hotmail.com>
2024-02-22 23:57:14 -0600

    trying to fix my integration test

--- src/test/java/Test/UserControllerIntegrationTest.java
+++ src/test/java/Test/UserControllerIntegrationTest.java
@@ -1,13 +1,18 @@
 package Test;
 
+import Dto.PasswordDTO;
 import Dto.UserDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,16 +28,31 @@ public class UserControllerIntegrationTest {
         this.objectMapper = objectMapper;
     }
 
-
     @Test
-    public void testCreateUser() throws Exception {
+    @Transactional
+    public void testCreateAndSetPasswordForUser() throws Exception {
+        // Step 1: Create a user
         UserDTO userDTO = new UserDTO();
         userDTO.setUsername("testuser");
         userDTO.setRole("USER");
+        String userJson = objectMapper.writeValueAsString(userDTO);
 
-        mockMvc.perform(post("/api/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(userDTO)))
-                .andExpect(status().isCreated());
+        MvcResult createUserResult = mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson))
+                        .andExpect(status().isCreated())
+                        .andReturn();
+
+        String responseString = createUserResult.getResponse().getContentAsString();
+        Long userId = JsonPath.read(responseString, "$.id");
+
+        // Step 2: Set the password for the newly created user
+        PasswordDTO passwordDTO = new PasswordDTO();
+        passwordDTO.setNewPassword("newSecurePassword");
+        mockMvc.perform(put("/api/users/" + userId + "/password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(passwordDTO)))
+                        .andExpect(status().isOk());
     }
+
 }
