9fddccc67c7236c1a26a4d1b27dbadc758e9edd6
Reece Nunez <reecenunez@hotmail.com>
2024-02-23 10:01:06 -0600

    had to create getCurrentUser() to my interface

--- src/main/java/com/nunezdev/inventory_manager/service/UserService.java
+++ src/main/java/com/nunezdev/inventory_manager/service/UserService.java
@@ -3,13 +3,17 @@ package com.nunezdev.inventory_manager.service;
 
 import com.nunezdev.inventory_manager.dto.UserDTO;
 import com.nunezdev.inventory_manager.entity.AppUser;
+import com.nunezdev.inventory_manager.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
-    AppUser createUser(String username, String password);
+
+    AppUser createUser(String username, String password, String rawPassword);
     void changePassword(AppUser appUser, String newPassword);
     AppUser findById(long id);
     UserDTO convertToDTO(AppUser appUser);
     AppUser convertToEntity(UserDTO userDTO);
+    AppUser getCurrentUser();
 }
