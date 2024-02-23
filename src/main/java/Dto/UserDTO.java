8f9e3a4c0c62e6df96eb64d2a041428bf07d5ee1
Reece Nunez <reecenunez@hotmail.com>
2024-02-22 22:53:24 -0600

    removed the UserDTO constructor and added the @AllArgsConstructor annotation

--- src/main/java/Dto/UserDTO.java
+++ src/main/java/Dto/UserDTO.java
@@ -1,23 +1,14 @@
 package Dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
+@AllArgsConstructor
 public class UserDTO {
     private long id;
     private String username;
     private String role;
-
-
-    public UserDTO(long id, String username, String role) {
-        this.id = id;
-        this.username = username;
-        this.role = role;
-    }
 }
