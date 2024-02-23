92a13c9076d37c6ad0541803c446fb983e1f57aa
Reece Nunez <reecenunez@hotmail.com>
2024-02-22 22:52:40 -0600

    removed changePassword from my controller and left it in my User Implementation file. rewrote setPassword

--- src/main/java/Controller/UserController.java
+++ src/main/java/Controller/UserController.java
@@ -1,8 +1,12 @@
 package Controller;
 
+import Dto.PasswordDTO;
 import Dto.UserDTO;
 import Entity.AppUser;
 import Service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,25 +15,28 @@ public class UserController {
 
     private final UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService){
         this.userService = userService;
     }
 
     @PostMapping
-    public UserDTO createUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
         AppUser appUser = userService.createUser(userDTO.getUsername(), userDTO.getRole());
-        return userService.convertToDTO(appUser);
+        UserDTO createdUser = userService.convertToDTO(appUser);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}/password")
-    public void setPassword(@PathVariable long id, @RequestBody String newPassword) {
-        AppUser appUser = userService.findById(id);
-        userService.changePassword(appUser, newPassword);
-    }
-
-    @PutMapping("/{id}/password")
-    public void changePassword(@PathVariable long id, @RequestBody String newPassword) {
-        AppUser appUser = userService.findById(id);
-        userService.changePassword(appUser, newPassword);
+    public ResponseEntity<?> setPassword(@PathVariable long id, @RequestBody PasswordDTO passwordDTO) {
+        try {
+            AppUser appUser = userService.findById(id);
+            if (appUser == null) {
+                return ResponseEntity.notFound().build();
+            }
+            userService.changePassword(appUser, passwordDTO.getNewPassword());
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while setting the password");
+        }
     }
 }
