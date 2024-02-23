97b45c757f931c5a2f1ba6c2ab50975116ce5085
Reece Nunez <reecenunez@hotmail.com>
2024-02-22 18:30:03 -0600

    added a User Entity file with appropriate content to manage access to the application

---
+++ src/main/java/Entity/User.java
@@ -0,0 +1,22 @@
+package Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String password;
+    private String role;
+}
