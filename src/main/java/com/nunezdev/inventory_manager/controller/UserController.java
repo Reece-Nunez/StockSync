047c17ca7572b0d50a2996e042064d12e4e35b9b
Reece Nunez <reecenunez@hotmail.com>
2024-02-23 15:56:33 -0600

    added the /create URL to createUser

--- src/main/java/com/nunezdev/inventory_manager/controller/UserController.java
+++ src/main/java/com/nunezdev/inventory_manager/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.nunezdev.inventory_manager.controller;
 
+import com.nunezdev.inventory_manager.dto.LoginRequest;
+import com.nunezdev.inventory_manager.dto.LoginResponse;
 import com.nunezdev.inventory_manager.dto.PasswordDTO;
 import com.nunezdev.inventory_manager.dto.UserDTO;
 import com.nunezdev.inventory_manager.entity.AppUser;
 import com.nunezdev.inventory_manager.entity.Role;
+import com.nunezdev.inventory_manager.security.JwtTokenProvider;
 import com.nunezdev.inventory_manager.service.RoleService;
 import com.nunezdev.inventory_manager.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,13 +23,17 @@ public class UserController {
 
     private final UserService userService;
     private final RoleService roleService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
 
-    public UserController(UserService userService, RoleService roleService){
+    public UserController(UserService userService, RoleService roleService, JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager) {
         this.userService = userService;
         this.roleService = roleService;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
 
         Role role = roleService.findByName(userDTO.getRoleName());
@@ -54,5 +65,19 @@ public class UserController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while setting the password");
         }
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<LoginResponse> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtTokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new LoginResponse(jwt));
     }
 }
