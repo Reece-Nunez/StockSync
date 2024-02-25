308b266bc5cadd6c53499f640e980b0ffb6303fb
Reece Nunez <reecenunez@hotmail.com>
2024-02-25 14:34:03 -0600

    had to add the type to password to hide the password. When new user is registered successfully it navigates back to login

--- capstone_angular/src/app/register/register.component.ts
+++ capstone_angular/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {UserService} from "../service/UserService";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -13,13 +14,14 @@ export class RegisterComponent {
     roleName: ''
   }
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   register(): void {
     this.userService.createUser(this.user).subscribe({
       next: (data) => {
         console.log(data);
         alert('User created successfully');
+        this.router.navigate(['/login']);
       },
       error: (error) => {
         console.log('There was an error!', error);
