489bf23cb3bd8305bcc3aa4b6bbe249146e2b0f4
Reece Nunez <reecenunez@hotmail.com>
2024-02-27 16:47:59 -0600

    had to change the options for categories to category.name to get the name instead of the Object to string

--- src/main/java/com/nunezdev/inventory_manager/dto/CategoryDTO.java
+++ src/main/java/com/nunezdev/inventory_manager/dto/CategoryDTO.java
@@ -1,14 +1,12 @@
 package com.nunezdev.inventory_manager.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class CategoryDTO {
 
     private Long id;
