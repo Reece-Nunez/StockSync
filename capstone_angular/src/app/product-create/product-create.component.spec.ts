6e7b7d4c12341232a5ce9661d577acdb67e2e2e0
Reece Nunez <reecenunez@hotmail.com>
2024-02-29 16:10:35 -0600

    had a successful product creation test!

--- capstone_angular/src/app/product-create/product-create.component.spec.ts
+++ capstone_angular/src/app/product-create/product-create.component.spec.ts
@@ -1,20 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProductCreateComponent } from './product-create.component';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ProductService } from "../service/product/product.service";
+import { Router } from "@angular/router";
+import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
-import {RouterTestingModule} from "@angular/router/testing";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {MatCardModule} from "@angular/material/card";
+import {of} from "rxjs";
 
-describe('ProductCreateComponent', () => {
+describe('ProductCreateComponent Simplified', () => {
   let component: ProductCreateComponent;
   let fixture: ComponentFixture<ProductCreateComponent>;
+  let productServiceMock: any;
+  let router: Router;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    productServiceMock = {
+      createProduct: jasmine.createSpy('createProduct').and.returnValue(of({
+        id: 1,
+        name: 'Test Product',
+        description: 'Test Description',
+        price: '20.00',
+        quantity: 10
+      }))
+    };
+
+    await TestBed.configureTestingModule({
       imports: [
         MatFormFieldModule,
         MatInputModule,
@@ -25,14 +39,35 @@ describe('ProductCreateComponent', () => {
         MatCardModule,
         HttpClientTestingModule
       ],
-      declarations: [ProductCreateComponent]
-    });
+      declarations: [ ProductCreateComponent ],
+      providers: [
+        { provide: ProductService, useValue: productServiceMock }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProductCreateComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+
+  it('should submit product and navigate to dashboard', (done) => {
+    spyOn(router, 'navigate');
+
+    component.product = ({
+      name: 'Test Product',
+      description: 'Test Description',
+      price: '20.00',
+      quantity: 10
+    });
+
+    component.submitProduct();
+
+    fixture.whenStable().then(() => {
+      expect(productServiceMock.createProduct).toHaveBeenCalled();
+      expect(router.navigate).toHaveBeenCalledWith(['/dashboard']);
+      done();
+    })
   });
 });
